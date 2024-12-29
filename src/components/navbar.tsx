21f5d550a4ae2f3a4bfb3c91418080c4f298d7cc
Frazix12 <frazixgaming22@gmail.com>
2024-12-29 13:43:26 +0600

    Refactor navbar component by removing unused Link import

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import { Menu, X } from "lucide-react";
 import { ThemeToggle } from "./theme-toggle";
